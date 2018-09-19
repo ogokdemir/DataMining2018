35585a2bd4733148e7f547b2757988b678016585
ogokdemir <ozan.gokdemir15@ncf.edu>
2018-09-19 00:07:26 -0400

    Algorithm optimized by implementing the transaction reduction ttechnique learnt from a research paper. Now it runs on the entire database of 100k entries.

--- A1/src/application.py
+++ A1/src/application.py
@@ -26,50 +26,56 @@ def read_and_process_file(filepath):
          
     del database[0:2]
     return list(map(frozenset,database))
-    
-data = read_and_process_file('C:\\Users\\Ozan Gokdemir\\Desktop\\DataMining2018\\A1\\data\\sorted.csv')
-          
-    
+  
+
+dat = read_and_process_file("C:\\Users\\Ozan Gokdemir\\Desktop\\DataMining2018\\A1\\data\\sorted.csv")    
+
+#print(len(dat))
+
 
 #finds the single item candidates in the dataset. 
 def findCandidates1(dataset):
+    print("first candidates are being found")
     cands = []
     for trans in dataset: 
         for item in trans:
-            if not [item] in cands:
-                cands.append([item])
+            if not {item} in cands:
+                cands.append({item})
     
     cands.sort()
     #returning frozensets so that these itemsets can be used as the keys of a dictionary(a.k.a python hashtable).
     return list(map(frozenset, cands))
     
     
-#print(findCandidates1(database))
-
+#print(len(findCandidates1(dat)))
+#print(findCandidates1(dat))
 
 #an attempt to optimize the the algorithm. 
 #idea is that a transaction that does not contain any frequent k-itemset is useless in subsequent scans.
 def transaction_reduction(frequentItemsets, dataset):
+    print("transaction_reduction called")
     for trans in dataset:
-        count = 0 
         for freqSet in frequentItemsets:
             if freqSet.issubset(trans):
-                count+=1 
-        if count == 0:
-            dataset.remove(trans)
+                break
+        dataset.remove(trans)
 
 
 #filters the candidates that satisfy the support requirement, keeps the valid ones and dumps the rest. 
-def findFrequentItemsets(dataset, supCriteria):
+def findFrequentItemsets(cands, dataset, supCriteria):
+    print("findfrequentitemsets called.")
     frequentItemsets = [] #stores only the candidates that satisy the support threshold. 
-    cands = findCandidates1(dataset)
     while(len(cands)>0):
         temp = []
         for c in cands: 
             if support(c, dataset) >= supCriteria:
                 temp.append(frozenset(c))
-                frequentItemsets.append(c)             
+                frequentItemsets.append(c)
+        before_removal = len(dataset)        
         transaction_reduction(frequentItemsets, dataset)
+        after_removal = len(dataset)
+        print("transaction reduction executed, " + str(before_removal - after_removal)+ "transactions removed.")
+        print(str(after_removal) + " transactions remaining.")
         cands = []
         
         for t in temp:
@@ -80,46 +86,50 @@ def findFrequentItemsets(dataset, supCriteria):
     return frequentItemsets
 
 
-
-
-#print(filterCandidates(findCandidates1(database), database, 0.5))
-
-
-supportCache = {}
-
 def support(itemset, dataset):
-    
-    if(itemset in supportCache):
-        return supportCache[itemset]
-    else:
-        countOfAppearance = 0  
-        for trans in dataset:
-            if itemset.issubset(trans):
-                countOfAppearance += 1
-        
-        supportCache[itemset] = countOfAppearance
-        return countOfAppearance
+    count = 0 
+    for trans in dataset:
+        if itemset.issubset(trans):
+            count += 1
+    return count
+
+
 
 def confidence(precedent, antecedent, database):  
     testset = precedent.union(antecedent)
+    
     return support(testset,database) / support(precedent, database)
 
 
-def findRules(database, minConfidence):
-    frequentItems = findFrequentItemsets(database, 2)
-    rules = []
+
+def findRules(frequentItems, database, minConfidence):
+    
+    rules = {}
     
     for f in frequentItems:
         frequentItems.remove(f)
         for u in range(len(frequentItems)):
-            if confidence(f, frequentItems[u], database) >= minConfidence:
+            conf = confidence(f, frequentItems[u], database)
+            if conf >= minConfidence:
                 if(len(f) <= len(frequentItems[u])):
-                    rules.append((f , frequentItems[u]))
+                    rule = frozenset([f, frequentItems[u]])
+                    rules[rule] = conf
     return rules
     
 
-rules = list(findRules(data, 0.5))
-for r in rules:
-    print(str(r[0]) + " ===> " + str(r[1]))
-    
-    
+firstCandidates = findCandidates1(dat)
+
+print("k1 candidates calculated, number is "+ str(len(firstCandidates)))
+
+frequentItemsets = findFrequentItemsets(firstCandidates, dat, 170)
+
+print("frequent itemsets calculated, number is "+ str(len(frequentItemsets)))
+
+rules = findRules(frequentItemsets, dat, 0.95)
+
+
+for r in rules.keys():
+    temp = list(r)
+    print(str(temp[0]) + "===>" + str(temp[1]) + " with confidence= " + str(rules[r])+"\n")
+
+print("rules calculated number is "+ str(len(rules)))
